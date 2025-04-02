5c5d651f1c6e47a3ffe9f5ef398d3676abecc4e5
Anderson Moroni <andersonmoroni92@gmail.com>
2025-04-02 18:32:50 -0300

    feat: updated user router add middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,9 +4,9 @@ const userController = require("../controllers/userController");
 const isLogged = require("../middleware/auth");
 
 router.get("/", isLogged, userController.getUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get("/:id", isLogged, userController.getUserById);
+router.post("/", isLogged, userController.createUser);
+router.put("/:id", isLogged, userController.updateUser);
+router.delete("/:id", isLogged, userController.deleteUser);
 
 module.exports = router;
